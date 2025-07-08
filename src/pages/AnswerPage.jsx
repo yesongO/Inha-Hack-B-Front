20cb447c12ba0686a8bf055dfb59ab67f65e9ddf
leedasom050115 <kiss9892@inha.edu>
2025-07-08 18:24:31 +0900

    엔서페이지 작성

--- src/pages/AnswerPage.jsx
+++ src/pages/AnswerPage.jsx
@@ -2,11 +2,73 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Layout from "../components/Layout";
+import Topbar from '../components/Topbar';
+import "../pages/AnswerPage.css";
 
 export default function AnswerPage() {
-    return (
-        <Layout>
+  const { questionId } = useParams();
+  const [question, setQuestion] = useState(null);
+  const [answerText, setAnswerText] = useState("");
 
-        </Layout>
-    );
+  useEffect(() => {
+    axios.get(`/question/questions/${questionId}/`)
+      .then((res) => {
+        console.log("받은 데이터:", res.data);
+        setQuestion(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, [questionId]);
+
+  const handleSubmit = () => {
+  const payload = {
+    user_id: parseInt(localStorage.getItem("userId")),
+    question_id: parseInt(questionId),
+    body: answerText
+  };
+
+  console.log("보내는 데이터:", payload);
+
+  axios.post('/answer/answers/', payload)
+    .then(() => {
+      alert("답변 등록 완료!");
+      setAnswerText("");
+    })
+    .catch(err => console.error("POST 오류:", err));
+};
+
+  return (
+    <div>
+      <Layout>
+        <Topbar/>
+
+        <div className="AnswerPageContainer">
+          {question && (
+            <>
+              <div className="QuestionBox">
+                <h2 className="QuestionTitle">{question.title}</h2>
+                
+                <p className="QuestionBody">{question.body}</p>
+              </div>
+               
+               <div className="NoteText">진심으로 읽고 마음으로 답해보아요</div>
+              <div className="AnswerFormBox">
+                
+                <textarea 
+                  className="AnswerTextarea"
+                  value={answerText}
+                  onChange={(e) => setAnswerText(e.target.value)}
+                  placeholder="답변을 입력하세요"
+                />
+                <div className="ButtonRow">
+                <button className="SubmitButton" onClick={handleSubmit}>
+                  답변 등록
+                </button>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      </Layout>
+    </div>
+  );
 }
